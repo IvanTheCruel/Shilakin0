77eaec8f2eadddb7b8d1efac2f9bcc5147776460
lvanTheCrazy <70589882+lvanTheCrazy@users.noreply.github.com>
2020-09-01 18:14:13 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char t;
+    cout << "Hello World!\n" << "enter symbol: ";
+    cin >> t;
+
+    cout << "\nyour symbol is " << t;
+    return 0;
+}
